29d3ebc068c95807e6dee4dfb32da87244d95fdd
Marece <wbmmusic@gmail.com>
2022-02-06 13:50:47 -0600

    0.0.14

--- public/artNetTc.js
+++ public/artNetTc.js
@@ -1,43 +1,44 @@
 var NanoTimer = require('nanotimer');
 const dgram = require('dgram');
 const { Buffer } = require('buffer');
-
 let sender = dgram.createSocket('udp4');
-var timerA = new NanoTimer();
-
-const opCodeHigh = 0x97
-const opCodeLow = 0
-const protVerLow = 14
-const protVerHigh = 0
-let consoleAddress = ''
 
 console.log('TOP OF CHILD');
 
-const id = new Buffer.from('Art-Net\0')
-const opCode = new Buffer.from([opCodeLow, opCodeHigh])
-const protVer = new Buffer.from([protVerHigh, protVerLow])
-const filler = new Buffer.from([0, 0])
-
-const header = Buffer.concat([id, opCode, protVer, filler])
-
-let count = 0;
-
-let hours = 0;
-let mins = 0
-let secs = 0;
-let frames = 0
-let framerate = 30
-
-let running = false
-let output = false
-let speed = 1
-
+// Timer Stuff
+var frameTimer = new NanoTimer();
 const delays = {
     24: '41666666n',
     25: '40000000n',
     29.97: '33333333n',
     30: '33333333n'
 }
+
+// Clock Variables
+let hours = 0;
+let mins = 0
+let secs = 0;
+let frames = 0
+let framerate = 30
+let running = false
+
+// Output Variables
+let consoleAddress = ''
+let output = false
+let speed = 1
+
+// artTimecode Packet Constants
+const opCodeHigh = 0x97
+const opCodeLow = 0
+const protVerLow = 14
+const protVerHigh = 0
+const id = new Buffer.from('Art-Net\0')
+const opCode = new Buffer.from([opCodeLow, opCodeHigh])
+const protVer = new Buffer.from([protVerHigh, protVerLow])
+const filler = new Buffer.from([0, 0])
+const header = Buffer.concat([id, opCode, protVer, filler])
+
+let outBuffer = Buffer.alloc(19)
 
 const makeTypeByte = () => {
     switch (framerate) {
@@ -67,11 +68,12 @@ const addZero = (num) => {
     return num
 }
 
-const makeClock = () => {
+const sendClockToFrontEnd = () => {
+    const clock = { time: `${addZero(hours)}:${addZero(mins)}:${addZero(secs)}:${addZero(frames)}`, rate: framerate }
+    process.send({ cmd: 'time', clock })
+}
 
-    // Increment Frames
-    frames++
-
+const checkValidTime = () => {
     // Increment Seconds
     if (frames >= framerate) {
         frames = 0
@@ -82,10 +84,10 @@ const makeClock = () => {
     if (secs > 59) {
         secs = 0
         mins++
+    }
 
-        ////////////////////////////////////  This isnt handled right.. well almost... what if we start at 0:0:0:0 or 10:0:0:0..
-        // what if 00:00:10:01 is passed
-        if (framerate === 29.97) {
+    if (framerate === 29.97) {
+        if (frames === 0 || frames === 1) {
             if (mins % 10 === 0) {
                 console.log('Devisible by ten');
             } else {
@@ -95,44 +97,51 @@ const makeClock = () => {
         }
     }
 
-
     // Increment hours
     if (mins > 59) {
         mins = 0
         hours++
         if (hours > 23) hours = 0
     }
-
-
-    const time = { time: `${addZero(hours)}:${addZero(mins)}:${addZero(secs)}:${addZero(frames)}`, rate: framerate }
-    if (consoleAddress !== '' && output) sender.send(Buffer.concat([header, makeTimeBytes()]), 6454, consoleAddress)
-    return time
 }
 
-const nextFrame = (dummy) => {
-    const clock = makeClock(count)
-    process.send({ cmd: 'time', clock })
-    count++
+const makeOutBuffer = () => {
+    //console.log('Make Output Buffer');
+    checkValidTime()
+    outBuffer = Buffer.concat([header, makeTimeBytes()])
 }
 
-const stopTimer = () => {
+const sendMsg = async() => {
+    return new Promise(async(resolve, reject) => {
+        sender.send(outBuffer, 6454, consoleAddress, () => resolve())
+    })
+}
+
+const sendFrame = async() => {
+    //console.log(outBuffer);
+    if (output) await sendMsg()
+    sendClockToFrontEnd()
+    frames++
+    makeOutBuffer()
+}
+
+const stopClock = () => {
     running = false
-    timerA.clearInterval()
+    frameTimer.clearInterval()
 }
 
-const startTimer = () => {
-    stopTimer()
+const startClock = () => {
+    stopClock()
     running = true
-    timerA.setInterval(nextFrame, '', delays[framerate], function() {
-        //timerA.clearInterval();
+    frameTimer.setInterval(sendFrame, '', delays[framerate], function() {
+        //frameTimer.clearInterval();
     });
 }
 
 const setFrameRate = (rate) => {
     framerate = rate
-    if (running) startTimer()
-    const clock = { time: `${addZero(hours)}:${addZero(mins)}:${addZero(secs)}:${addZero(frames)}`, rate: framerate }
-    process.send({ cmd: 'time', clock })
+    if (running) startClock()
+    sendClockToFrontEnd()
     return framerate
 }
 
@@ -143,8 +152,11 @@ const handleTime = (time) => {
     secs = time[2]
     frames = time[3]
 
-    const clock = { time: `${addZero(hours)}:${addZero(mins)}:${addZero(secs)}:${addZero(frames)}`, rate: framerate }
-    process.send({ cmd: 'time', clock })
+    // sendClockToFrontEnd happens b4 makeOutBuffer so user sees what they typed
+    // DROP FRAME If in 29.97 they type 00:01:00:00 they will see that but outBuffer will be 00:01:00:02
+    sendClockToFrontEnd()
+    makeOutBuffer()
+    console.log(hours, mins, secs, frames);
 }
 
 process.on('message', (msg) => {
@@ -178,9 +190,9 @@ process.on('message', (msg) => {
         case 'state':
             //console.log('state Command', msg.state);
             if (msg.state === 'run') {
-                startTimer()
+                startClock()
             } else if (msg.state === 'stop') {
-                stopTimer()
+                stopClock()
             }
             process.send({ cmd: 'state', state: msg.state })
             break;
@@ -194,3 +206,5 @@ process.on('message', (msg) => {
             break;
     }
 })
+
+makeOutBuffer()
