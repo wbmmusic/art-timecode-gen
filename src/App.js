b424d46d7ad8af272109e69974a91695ad780a04
Marece <wbmmusic@gmail.com>
2022-08-17 12:53:16 -0500

    0.0.17

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Fragment } from "react";
 import Top from "./components/Top";
 import Updates from "./Updates";
-
+//
 function App() {
-  return (
-    <Fragment>
-      <Top />
-      <Updates />
-    </Fragment>
-  );
+    return ( <
+        Fragment >
+        <
+        Top / >
+        <
+        Updates / >
+        <
+        /Fragment>
+    );
 }
 
 export default App;
